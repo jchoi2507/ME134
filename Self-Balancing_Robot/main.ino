f8a038a45db59bade8570370c91295996142ba12
Jacob Choi <jchoi2507@gmail.com>
2023-11-03 02:07:11 -0400

    Update main.ino

--- Self-Balancing_Robot/main.ino
+++ Self-Balancing_Robot/main.ino
@@ -11,28 +11,41 @@ Jacob Choi 10/19/2023
 Adafruit_MPU6050 mpu;
 
 /* GPIO */
-#define PWM 27
-#define CW 26
-#define CCW 25
+#define PWM 35 //27
+#define CW 32 //26
+#define CCW 33 //25
 
-#define ENCODER_1 33
-#define ENCODER_2 32
+//#define ENCODER_1 33
+//#define ENCODER_2 32
 
 /* PWM */
 #define MAX_DUTY 225
 #define MIN_DUTY 0
 
+/* PID */
+float kp = 70;
+float kd = 5000; // 5000
+float ki = 0;
+float deltaTime = 5000;
+
+int targetAngle = 0;
+float e_integral = 0;
+float e_derivative;
+float u;
+int PID_signal;
+
+const float ANGLE_THRESHOLD = 15.00; // operating range (degrees)
+long currTime, prevTime = 0;
+float currError, prevError;
+
 /* Encoder */
-int encoder_1;
-int encoder_2;
-volatile int encoderCounter = 0; // encoder value
+//int encoder_1;
+//int encoder_2;
+//volatile int encoderCounter = 0; // encoder value
 
 /* MPU6050 IMU */
-float initial_pitch;
-float final_pitch;
-float delta_pitch;
-const float MPU6050_OFFSET = 9.0;
-const float MPU6050_THRESHOLD = 2.00;
+float currAngle;
+const float MPU6050_OFFSET = 3.8;
 
 void setup() {
   Serial.begin(115200);
@@ -43,26 +56,66 @@ void setup() {
       delay(10);
     }
   }
-  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+  mpu.setAccelerometerRange(MPU6050_RANGE_2_G);
   mpu.setGyroRange(MPU6050_RANGE_500_DEG);
-  mpu.setFilterBandwidth(MPU6050_BAND_5_HZ);
+  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
   delay(100);
 
   pinMode(PWM, OUTPUT);
   pinMode(CW, OUTPUT);
   pinMode(CCW, OUTPUT);
-  pinMode(ENCODER_1, INPUT);
-  pinMode(ENCODER_2, INPUT);
+  //pinMode(ENCODER_1, INPUT);
+  //pinMode(ENCODER_2, INPUT);
 
   reactionWheel();
 }
 
 void loop() { }
 
-void motorController(bool cw, int duty_percentage) {
-  int duty_cycle = MAX_DUTY * duty_percentage/100; // converting percentage -> 0 ~ 225 range
-  analogWrite(PWM, duty_cycle);
+void reactionWheel() {
+  while (true) {
+    currTime = micros();
+    
+    if (currTime - prevTime > deltaTime) { // PID loop frequency: 200Hz
+      readSensor();
+      if (abs(currAngle) < ANGLE_THRESHOLD) { // start PID loop
+          // Proportional
+          currError = targetAngle - currAngle;
 
+          // Integral
+          //e_integral = e_integral + currError*deltaTime;
+          //e_integral = constrain(e_integral, -127, 127); // accounting for integral windup
+
+          //if (currError < 0) { targetAngle -= 0.0001; } // dithering
+          //else { targetAngle += 0.0001; }
+
+          // Derivative
+          e_derivative = (currError - prevError)/deltaTime;
+          prevError = currError;
+
+          // Control signal
+          u = kp * currError + kd * e_derivative + ki * e_integral;
+          PID_signal = constrain(u, -255, 255);
+
+          if (PID_signal > 0) { // turn motor CCW
+            motorController(true, abs(PID_signal));
+            //Serial.println("TURNING CW: ");
+            //Serial.println(PID_signal);
+          }
+
+          else if (PID_signal < 0) { // turn motor CW
+            motorController(false, abs(PID_signal));
+            //Serial.println("TURNING CCW: ");
+            //Serial.println(PID_signal);
+          }
+      }
+      else { analogWrite(PWM, MIN_DUTY); } // turn off motor if outside range
+      prevTime = currTime;
+    }
+  }
+}
+
+void motorController(bool cw, int PID_signal) {
   if (cw) { // CW rotation
     digitalWrite(CW, HIGH);
     digitalWrite(CCW, LOW);
@@ -74,49 +127,11 @@ void motorController(bool cw, int duty_percentage) {
     digitalWrite(CW, LOW);
     //delay(pause_ms);
   }
+  analogWrite(PWM, PID_signal);
 }
 
-void reactionWheel() {
-  //sensors_event_t a, g, temp;
-  //mpu.getEvent(&a, &g, &temp);
-  //initial_pitch = MPU6050_OFFSET + atan2(-a.acceleration.x , sqrt(a.acceleration.y * a.acceleration.y + a.acceleration.z * a.acceleration.z)) * 180.0 / PI;
-
-  while (true) {
+void readSensor() {
     sensors_event_t a, g, temp;
     mpu.getEvent(&a, &g, &temp);
-  
-    final_pitch = MPU6050_OFFSET + atan2(-a.acceleration.x , sqrt(a.acceleration.y * a.acceleration.y + a.acceleration.z * a.acceleration.z)) * 180.0 / PI;
-    //delta_pitch = final_pitch - initial_pitch;
-
-    /*
-    Serial.print("INITIAL: ");
-    Serial.println(initial_pitch);
-    Serial.print("FINAL: ");
-    Serial.println(final_pitch);
-    delay(500); */
-
-    if (final_pitch > MPU6050_THRESHOLD) {
-      motorController(true, 45); // turn CW
-
-      while (final_pitch > MPU6050_THRESHOLD) { // turn motor until balanced
-        sensors_event_t a, g, temp;
-        mpu.getEvent(&a, &g, &temp);
-        final_pitch = MPU6050_OFFSET + atan2(-a.acceleration.x , sqrt(a.acceleration.y * a.acceleration.y + a.acceleration.z * a.acceleration.z)) * 180.0 / PI;
-      }
-      analogWrite(PWM, MIN_DUTY);
-      //initial_pitch = final_pitch; // update vars
-    }
-
-    else if (final_pitch < -MPU6050_THRESHOLD) { 
-      motorController(false, 45); // turn CCW
-
-      while (final_pitch < -MPU6050_THRESHOLD) { // turn motor until balanced
-        sensors_event_t a, g, temp;
-        mpu.getEvent(&a, &g, &temp);
-        final_pitch = MPU6050_OFFSET + atan2(-a.acceleration.x , sqrt(a.acceleration.y * a.acceleration.y + a.acceleration.z * a.acceleration.z)) * 180.0 / PI;
-      }
-      analogWrite(PWM, MIN_DUTY);
-      //initial_pitch = final_pitch; // update vars
-    }
-  }
+    currAngle = MPU6050_OFFSET + atan2(-a.acceleration.x , sqrt(a.acceleration.y * a.acceleration.y + a.acceleration.z * a.acceleration.z)) * 180.0 / PI;
 }
